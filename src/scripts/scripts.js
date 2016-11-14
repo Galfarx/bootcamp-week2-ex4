769a1cc9f93675743a453c20bf0078d320b0368e
Dominik Kubis <kubis.dominik1@gmail.com>
2016-11-14 20:31:05 +0100

    [BUGFIX] - move key to string conversion after if statement

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -5,21 +5,23 @@ function createData(obj) {
     var data = obj;
 
     function getObj(key) {
-        key = '' + key;
 
         if (!(key in data)){
             throw new Error('Podany klucz nie istnieje');
         }
 
+        key = '' + key;
+
         return data[key]
     }
 
     function setObj(key, value) {
-        key = '' + key;
 
         if (!key || !value) {
             throw new Error('Należy opodać klucz oraz wartość do ustawienia!');
         }
+
+        key = '' + key;
 
         data[key] = value;
     }
